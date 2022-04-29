a960a93e389723c0b0f5ea6a831e2fb280789323
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-04-29 21:06:10 +0900

    Add bottom navigation

--- components/base.tsx
+++ components/base.tsx
@@ -1,10 +1,16 @@
-import { Center, Heading, Box, VStack, Text, Flex } from '@chakra-ui/react'
+import { Center, Heading, Box, VStack, Text, Flex, HStack } from '@chakra-ui/react'
 import Link from 'next/link'
 
 const Base = ({ children }) => {
   return (
     <Box background="#ECECEC">
-      <VStack w="200px" position="fixed" height="full" justify="center">
+      <VStack
+        w="200px"
+        position="fixed"
+        height="full"
+        justify="center"
+        display={['none', 'none', 'flex']}
+      >
         <Link href="/">
           <Text cursor="pointer">About Me</Text>
         </Link>
@@ -35,6 +41,25 @@ const Base = ({ children }) => {
           <>{children}</>
         </Box>
       </Center>
+      <HStack
+        w="100%"
+        position="fixed"
+        bottom="0px"
+        justify="center"
+        display={['flex', 'flex', 'none']}
+        backgroundColor="white"
+        border="1px solid"
+      >
+        <Link href="/">
+          <Text cursor="pointer">About Me</Text>
+        </Link>
+        <Link href="/blog">
+          <Text cursor="pointer">Blog</Text>
+        </Link>
+        <Link href="/projects">
+          <Text cursor="pointer">Projects</Text>
+        </Link>
+      </HStack>
     </Box>
   )
 }
