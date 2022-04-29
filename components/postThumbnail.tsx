0909e2bf391dce5d50551e247af5f0c7a2d44c67
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-04-29 19:26:31 +0900

    Make post thumbnail factory

--- components/postThumbnail.tsx
+++ components/postThumbnail.tsx
@@ -1,18 +1,46 @@
+import Image from 'next/image'
+
 import { Center, Heading, Box } from '@chakra-ui/react'
 import Link from 'next/link'
+import dayjs from 'dayjs'
+
+const getThumbnailUrl = (post) => {
+  for (let node of post.fields.content.content) {
+    if (node.nodeType == 'embedded-asset-block') {
+      return node.data.target.fields.file.url
+    }
+  }
+}
+
+const thumbnailFactory = (post) => {
+  for (let node of post.fields.content.content) {
+    if (node.nodeType == 'embedded-asset-block') {
+      return (
+        <Image
+          src={`https:${node.data.target.fields.file.url}`}
+          layout="fill"
+          objectFit="contain"
+        ></Image>
+      )
+    }
+  }
+}
 
 const PostThumbnail = ({ post }) => {
+  const createdAt = dayjs(post.sys.createdAt)
+
   return (
-    <li key={post.sys.id}>
-      <Box border="1px solid" p="6" mt="6" mb="6" rounded="md">
-        <Link href={`/${post.fields.slug}`}>
-          <a>
-            <h2>{post.fields.title}</h2>
-            <p>{post.sys.createdAt}</p>
-          </a>
-        </Link>
-      </Box>
-    </li>
+    <Box key={post.sys.id} border="2px solid" p="6" mt="6" mb="6" rounded="md">
+      <Link href={`/${post.fields.slug}`}>
+        <a>
+          <Box height="200px" position="relative">
+            {thumbnailFactory(post)}
+          </Box>
+          <h2>{post.fields.title}</h2>
+          <p>Posted on {createdAt.format('DD/MM/YYYY')}</p>
+        </a>
+      </Link>
+    </Box>
   )
 }
 
