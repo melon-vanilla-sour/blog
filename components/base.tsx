dff13857038640e9f89d6ebcd8aab22a83ac8988
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-05-01 03:49:32 +0900

    Refactor navigation

--- components/base.tsx
+++ components/base.tsx
@@ -1,30 +1,18 @@
 import { Center, Heading, Box, VStack, Text, Flex, HStack } from '@chakra-ui/react'
 import Link from 'next/link'
 
+import MobileNavigation from './MobileNavigation'
+import Navigation from './Navigation'
+
 const Base = ({ children }) => {
   return (
-    <Box background="#ECECEC">
-      <VStack
-        w="200px"
-        position="fixed"
-        height="full"
-        justify="center"
-        display={['none', 'none', 'flex']}
-      >
-        <Link href="/">
-          <Text cursor="pointer">About Me</Text>
-        </Link>
-        <Link href="/blog">
-          <Text cursor="pointer">Blog</Text>
-        </Link>
-        <Link href="/projects">
-          <Text cursor="pointer">Projects</Text>
-        </Link>
-      </VStack>
+    <Box>
+      <Navigation />
       <Center flex="1">
         <Box
           boxShadow="24px 24px 48px #d1d1d1, -24px -24px 48px #ffffff"
-          p="6"
+          paddingTop="60px"
+          paddingBottom="60px"
           mt="6"
           mb="6"
           rounded="md"
@@ -35,31 +23,10 @@ const Base = ({ children }) => {
           alignItems="center"
           background="white"
         >
-          <Heading textDecoration="underline" textUnderlineOffset="5px">
-            Melon Vanilla Sour
-          </Heading>
-          <>{children}</>
+          <Box>{children}</Box>
         </Box>
       </Center>
-      <HStack
-        w="100%"
-        position="fixed"
-        bottom="0px"
-        justify="center"
-        display={['flex', 'flex', 'none']}
-        backgroundColor="white"
-        border="1px solid"
-      >
-        <Link href="/">
-          <Text cursor="pointer">About Me</Text>
-        </Link>
-        <Link href="/blog">
-          <Text cursor="pointer">Blog</Text>
-        </Link>
-        <Link href="/projects">
-          <Text cursor="pointer">Projects</Text>
-        </Link>
-      </HStack>
+      <MobileNavigation></MobileNavigation>
     </Box>
   )
 }
