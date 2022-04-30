696b314d5763463868aa4126e78dcac33109cb57
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-05-01 03:49:20 +0900

    Remove heading margins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,6 @@ const theme = extendTheme({
   components: {
     Heading: {
       baseStyle: {
-        marginTop: '15px',
-        marginBottom: '15px',
-        // textDecoration: 'underline',
-        // textUnderlineOffset: '5px',
         textAlign: 'center',
       },
     },
