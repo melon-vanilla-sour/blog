cb4a355372a12dcfd4af80db7a0f4b2bb85dbffa
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-05-07 03:54:20 +0900

    Fixed next/script placement from _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,19 @@ import '@fontsource/rubik/800.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import theme from '../../theme'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      {/* Remove overscrolling behavior for Safari */}
+      <Script id="safari-overscroll-none">
+        {`document.addEventListener('touchmove', function(event){
+              event.preventDefault();
+            }, { passive: false });
+          `}
+      </Script>
     </ChakraProvider>
   )
 }
