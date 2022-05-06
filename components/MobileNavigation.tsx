45024788bfa8a9962d22e21c0a227cdab8a1f380
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-05-06 17:11:56 +0900

    Fix mobile navigation click area

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,26 +1,48 @@
-import { Center, Heading, Box, VStack, Text, Flex, HStack } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import {
+  Center,
+  Heading,
+  Box,
+  VStack,
+  Text,
+  Flex,
+  HStack,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import Link from 'next/link'
 
 const MobileNavigation = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <HStack
       w="100%"
       position="fixed"
       bottom="0px"
       display={['flex', 'flex', 'none']}
-      backgroundColor="white"
-      boxShadow="dark-lg"
+      boxShadow="2xl"
       justify="space-around"
+      background={useColorModeValue('white', 'gray.800')}
     >
       <Link href="/">
-        <Text cursor="pointer">About Me</Text>
+        <Text cursor="pointer" w={'100%'} textAlign="center">
+          About Me
+        </Text>
       </Link>
       <Link href="/posts">
-        <Text cursor="pointer">Blog</Text>
+        <Text cursor="pointer" w={'100%'} textAlign="center">
+          Posts
+        </Text>
       </Link>
       <Link href="/projects">
-        <Text cursor="pointer">Projects</Text>
+        <Text cursor="pointer" w={'100%'} textAlign="center">
+          Projects
+        </Text>
       </Link>
+      <IconButton aria-label="Toggle Mode" onClick={toggleColorMode}>
+        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+      </IconButton>
     </HStack>
   )
 }
