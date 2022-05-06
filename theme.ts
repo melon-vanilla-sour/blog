f8393b5113d0674c19551ea0f7f652e7846d7126
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-05-07 00:43:19 +0900

    No overscroll behavior

--- theme.ts
+++ theme.ts
@@ -6,7 +6,7 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   styles: {
     global: {
-      html: {
+      'html, body': {
         overscrollBehaviorY: 'none',
       },
     },
