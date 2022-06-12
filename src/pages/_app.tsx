f8663aca7a6fc561027afac4a14f262f43d9c57d
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-06-13 03:39:06 +0900

    Add main to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,16 @@ import '@fontsource/rubik/800.css'
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+import Main from '../../components/main'
+
 import theme from '../../theme'
-import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-      {/* Remove overscrolling behavior for Safari */}
-      {/* <Script id="safari-overscroll-none">
-        {`document.addEventListener('touchmove', function(event){
-              event.preventDefault();
-            }, { passive: false });
-          `}
-      </Script> */}
+      <Main>
+        <Component {...pageProps} />
+      </Main>
     </ChakraProvider>
   )
 }
