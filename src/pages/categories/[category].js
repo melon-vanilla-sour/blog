bb4e60487b3c325c6e22e20421e042509ca40f0b
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-07-01 04:15:53 +0900

    Add post pages for each category

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -1,8 +1,29 @@
 import { buildClient } from '../../lib/contentful'
+import Card from '../../components/Card'
 import Pagination from '../../components/Pagination'
-import { Heading } from '@chakra-ui/react'
+import { Heading, Link, Button, Grid, GridItem } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 const client = buildClient()
+// const router = useRouter()
+// const { pid } = router.query
+
+const capitalizeString = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+const removeEmptyFields = (categories) => {
+  return [...categories].filter((category) => category.fields != null)
+}
+
+// Since 'category' isn't a required field for post, some category entries don't have a 'fields' key
+const removeDuplicates = (categories) => {
+  const array = []
+  categories.map((category) => {
+    if (!array.includes(category.fields.category)) {
+      array.push(category.fields.category)
+    }
+  })
+  return array
+}
 
 export const getStaticPaths = async () => {
   const { items: categories } = await client.getEntries({
@@ -10,18 +31,10 @@ export const getStaticPaths = async () => {
     order: '-sys.createdAt',
     select: 'fields.category',
   })
-  // Since 'category' isn't a required field for post, some category entries don't have a 'fields' key
-  categories.filter((category) => {
-    category.fields != null
-  })
-  const nonDuplicateCategories = []
-  categories.map((category) => {
-    if (!nonDuplicateCategories.includes(category.fields.category)) {
-      nonDuplicateCategories.push(category.fields.category)
-    }
-  })
+  const nonEmptyCategories = removeEmptyFields(categories)
+  const uniqueNonEmptyCategories = removeDuplicates(nonEmptyCategories)
   const paths = []
-  nonDuplicateCategories.map((category) => {
+  uniqueNonEmptyCategories.map((category) => {
     paths.push({ params: { category: category } })
   })
   return {
@@ -29,19 +42,35 @@ export const getStaticPaths = async () => {
     fallback: false,
   }
 }
-
 export const getStaticProps = async ({ params }) => {
+  const { items: posts } = await client.getEntries({
+    content_type: 'post',
+    order: '-sys.createdAt',
+    'fields.category[in]': params.category,
+  })
   return {
     props: {
       category: params.category,
+      posts: posts,
     },
   }
 }
 
-function Category({ category }) {
+function Category({ category, posts }) {
   return (
     <>
-      <Heading my={8}>{category}</Heading>
+      <Heading my={8}>{capitalizeString(category)}</Heading>
+      <Grid templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' }} gap={6}>
+        {posts &&
+          posts.map((post, index) => (
+            <GridItem>
+              <Card post={post} index={index} key={post.sys.id}></Card>
+            </GridItem>
+          ))}
+        <Link href="/posts/1">
+          <Button w={40}>View all posts</Button>
+        </Link>
+      </Grid>
       <Pagination></Pagination>
     </>
   )
