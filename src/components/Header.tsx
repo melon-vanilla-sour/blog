dace9d9c232086090ed3cfde9e034b88d4d078df
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2022-07-08 00:34:58 +0900

    Redesign Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,70 @@
-import { Heading, Flex, Spacer } from '@chakra-ui/react'
+import { Heading, Flex, Box, Image } from '@chakra-ui/react'
 import { useColorMode } from '@chakra-ui/color-mode'
 import Link from 'next/link'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { IconButton } from '@chakra-ui/button'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const [currentPage, setCurrentPage] = useState('')
+  const router = useRouter()
+  const pathName = () => {
+    if (router.pathname.includes('posts')) {
+      return 'posts'
+    }
+    if (router.pathname.includes('projects')) {
+      return 'projects'
+    }
+    return 'about'
+  }
+  useEffect(() => {
+    setCurrentPage(pathName())
+  }, [router])
 
   return (
-    <Flex
-      w="full"
-      display={{ base: 'none', sm: 'flex' }}
-      alignItems="center"
-      paddingX={8}
-      className="navigation"
-    >
-      <Heading size="sm">Melon Vanilla Sour</Heading>
-      <Spacer />
-      <Link href="/">
-        <Heading size="sm" cursor="pointer">
-          About
-        </Heading>
-      </Link>
-      <Spacer />
-      <Link href="/posts/1">
-        <Heading size="sm" cursor="pointer">
-          Posts
-        </Heading>
-      </Link>
-      <Spacer />
-      <Link href="/projects">
-        <Heading size="sm" cursor="pointer">
-          Projects
-        </Heading>
-      </Link>
-      <Spacer />
-      <IconButton aria-label="Toggle Mode" onClick={toggleColorMode}>
-        {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-      </IconButton>
-    </Flex>
+    <>
+      <Flex margin="0 auto">
+        <Image src="/melon-sour.ico" w="64px" h="64px"></Image>
+        <Heading fontSize="4xl">Melon Sour</Heading>
+      </Flex>
+      <Flex
+        w="350px"
+        display={{ base: 'none', sm: 'flex' }}
+        justifyContent="space-between"
+        paddingX={8}
+        className="navigation"
+        mx="auto"
+        my={5}
+      >
+        <Link href="/">
+          <Heading
+            size="md"
+            cursor="pointer"
+            fontWeight={currentPage == 'about' ? 'bold' : 'regular'}
+          >
+            About
+          </Heading>
+        </Link>
+        <Link href="/posts/1">
+          <Heading
+            size="md"
+            cursor="pointer"
+            fontWeight={currentPage == 'posts' ? 'bold' : 'regular'}
+          >
+            Posts
+          </Heading>
+        </Link>
+        <Link href="/projects">
+          <Heading
+            size="md"
+            cursor="pointer"
+            fontWeight={currentPage == 'projects' ? 'bold' : 'regular'}
+          >
+            Projects
+          </Heading>
+        </Link>
+      </Flex>
+      <Box className="underline" border="2.5px solid #444444" bg="#444444"></Box>
+    </>
   )
 }
 
