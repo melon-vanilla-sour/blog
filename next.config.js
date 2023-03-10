d67cd591394cf3944d43fc030ff14cc22e4ca288
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2023-03-10 22:02:37 +0900

    Install plaiceholder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+import { withPlaiceholder } from '@plaiceholder/next'
+
+module.exports = withPlaiceholder({
   images: {
     domains: ['images.ctfassets.net'],
   },
   experimental: { images: { allowFutureImage: true } },
-}
+})
