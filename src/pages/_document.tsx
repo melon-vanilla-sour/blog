a56e7a7df4808bf8eccab6a7652a11e71a83d392
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-02-11 19:06:24 +0900

    try out ogp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,12 @@ import theme from '../../theme'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:title" content="Melon Sour" />
+        <meta property="og:url" content="https://www.melonsour.com" />
+        <meta property="og:image" content="/melon-sour.ico" />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
