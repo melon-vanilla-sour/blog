facd975daa0db85c0e206375d3b7ea462fd7ed2a
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-02-17 20:33:12 +0900

    fix pagination click area

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,28 +6,31 @@ const Pagination = ({ totalPages, currentPage }: { totalPages: number, currentPa
   return (
     <Flex mt={8} justifyContent="center">
       {[...Array(totalPages)].map((page, index) => (
-        <Box
-          borderRadius="50%"
-          backgroundColor={useColorModeValue('white', 'whiteAlpha.200')}
-          w="45px"
-          h="45px"
-          lineHeight="45px"
-          textAlign="center"
-          boxShadow="lg"
-          key={index}
-          mx={2}
-          style={{ caretColor: 'transparent' }}
-          border='1px solid'
-          borderColor={index + 1 == currentPage ? useColorModeValue('blackAlpha.400', 'whiteAlpha.600') : 'transparent'}
-          _hover={{
-            boxShadow: 'xs',
-            opacity: '1',
-            borderColor: useColorModeValue('blackAlpha.400', 'whiteAlpha.600'),
-            transition: '0.2s ease-in-out',
-          }}
-        >
-          <Link href={`/posts/${index + 1}`}>{(index + 1).toString()}</Link>
-        </Box>
+        <Link href={`/posts/${index + 1}`}>
+          <Box
+            borderRadius="50%"
+            backgroundColor={useColorModeValue('white', 'whiteAlpha.200')}
+            w="45px"
+            h="45px"
+            lineHeight="45px"
+            textAlign="center"
+            boxShadow="lg"
+            key={index}
+            mx={2}
+            style={{ caretColor: 'transparent' }}
+            border='1px solid'
+            borderColor={index + 1 == currentPage ? useColorModeValue('blackAlpha.400', 'whiteAlpha.600') : 'transparent'}
+            _hover={{
+              boxShadow: 'xs',
+              opacity: '1',
+              borderColor: useColorModeValue('blackAlpha.400', 'whiteAlpha.600'),
+              transition: '0.2s ease-in-out',
+            }}
+            cursor='pointer'
+          >
+            {(index + 1).toString()}
+          </Box>
+        </Link>
       ))
       }
     </Flex >
