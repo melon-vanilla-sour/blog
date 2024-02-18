37b6257bf066bf6d29a68bbd8697784bae96eb56
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-02-18 15:54:42 +0900

    refactor projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,96 +1,14 @@
 import {
-  Box,
-  Heading,
-  Text,
-  useColorModeValue,
-  Flex,
   Grid,
-  GridItem,
-  Icon,
-  Image,
 } from '@chakra-ui/react'
-import Link from 'next/link'
-import { BiWrench } from 'react-icons/bi'
-// import Image from 'next/image'
-
-import { CardTextContainer } from '../components/Card'
+import MediumCard from '../components/MediumCard'
 
 function Projects() {
   return (
-    <>
-      <Grid my={8} templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' }} gap={6}>
-        <GridItem>
-          <Link href="/post/holoview">
-            <Box className="card">
-              <Flex direction="column">
-                <Box
-                  as={Image}
-                  src={'projects/holoview-thumbnail.png?fm=webp&h=600'}
-                  alt="Post Thumbnail"
-                  position="relative"
-                  filter={'saturate(130%) brightness(110%)'}
-                  w="420px"
-                  h="240px"
-                  objectFit="cover"
-                ></Box>
-                <CardTextContainer>
-                  <Heading
-                    fontSize={{ base: 'md', md: 'lg' }}
-                    textAlign="start"
-                    // Don't want to cause height shift within 2 lines, somehow isn't 2.4em (1.2 * 2)
-                    // minH="2.6em"
-                    noOfLines={2}
-                  >
-                    Holoview
-                  </Heading>
-
-                  <Flex alignItems="center">A page to watch Hololive VTubers</Flex>
-                  <Flex alignItems="center">
-                    <Icon as={BiWrench} marginEnd={2} />
-                    React, Chakra UI, Vercel
-                  </Flex>
-                </CardTextContainer>
-              </Flex>
-            </Box>
-          </Link>
-        </GridItem>
-        <GridItem>
-          <Link href="/post/making-my-blog-with-nextjs-vercel-and-contentful">
-            <Box className="card">
-              <Flex direction="column">
-                <Box
-                  as={Image}
-                  src={'projects/blog-thumbnail.png'}
-                  alt="Post Thumbnail"
-                  position="relative"
-                  filter={'saturate(130%) brightness(110%)'}
-                  w="420px"
-                  h="240px"
-                  objectFit="cover"
-                ></Box>
-                <CardTextContainer>
-                  <Heading
-                    fontSize={{ base: 'md', md: 'lg' }}
-                    textAlign="start"
-                    // Don't want to cause height shift within 2 lines, somehow isn't 2.4em (1.2 * 2)
-                    // minH="2.6em"
-                    noOfLines={2}
-                  >
-                    Blog
-                  </Heading>
-
-                  <Flex alignItems="center">A static generated blog</Flex>
-                  <Flex alignItems="center">
-                    <Icon as={BiWrench} marginEnd={2} />
-                    Next.js, Chakra UI, Contentful, Vercel
-                  </Flex>
-                </CardTextContainer>
-              </Flex>
-            </Box>
-          </Link>
-        </GridItem>
-      </Grid>
-    </>
+    <Grid my={8} templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' }} gap={6}>
+      <MediumCard link='/post/holoview' imgSrc='projects/holoview-thumbnail.png?fm=webp&h=600' name='Holoview' description='A page to watch Hololive VTubers' stack={['React', 'Chakra UI', 'Vercel']}></MediumCard>
+      <MediumCard link='/post/making-my-blog-with-nextjs-vercel-and-contentful' imgSrc='projects/blog-thumbnail.png' name='Blog' description='A static generated blog' stack={['Next.js', 'Chakra UI', 'Contentful', 'Vercel']}></MediumCard>
+    </Grid>
   )
 }
 
