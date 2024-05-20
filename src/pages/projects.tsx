cdb561a72f714d5cd4f49a4ac946a0bd8c7e7288
melon-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2024-05-20 13:07:43 +0900

    fix link

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -8,7 +8,7 @@ function Projects() {
   return (
     <Grid my={8} templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' }} gap={6}>
       <MediumCard link='/post/holoview' imgSrc='projects/holoview-thumbnail.png?fm=webp&h=600' name='Holoview' description='A page to watch Hololive VTubers' stack={['React', 'Chakra UI', 'Vercel']}></MediumCard>
-      <MediumCard link='/post/making-my-blog-with-nextjs-vercel-and-contentful' imgSrc='projects/blog-thumbnail.png' name='Blog' description='A static generated blog' stack={['Next.js', 'Chakra UI', 'Contentful', 'Vercel']}></MediumCard>
+      <MediumCard link='/post/nextjs-contentful-blog' imgSrc='projects/blog-thumbnail.png' name='Blog' description='A static generated blog' stack={['Next.js', 'Chakra UI', 'Contentful', 'Vercel']}></MediumCard>
     </Grid>
   )
 }
