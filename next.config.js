5b35e7c54c6ab0d643dd43f7934c128290d875b6
melon-vanilla-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2026-08-08 05:39:28 +0900

    feat: Prepare static export for Cloudflare Pages migration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  images: {
-    domains: ['melon-sour-blog-images.s3.amazonaws.com'],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/posts/1',
-        permanent: true,
-      },
-    ]
-  },
+  trailingSlash: true,
 }
