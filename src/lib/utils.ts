21a6f7ac043e660a1e52c866d652642270a02805
melon-vanilla-sour <93722184+melon-vanilla-sour@users.noreply.github.com>
2026-08-11 18:09:37 +0900

    feat: update image hosting url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,7 @@ export const getFirstLines = (markdown: string) => {}
 
 export const getImageUrls = (markdown) => {
   // const pattern = /https:\/\/melon-sour-blog-images\.s3\.amazonaws\.com\/[a-zA-Z0-9-.]+/g
-  const pattern = /https:\/\/d2pdw9m8ako0ty\.cloudfront\.net\/[a-zA-Z0-9-.]+/g
+  const pattern = /https:\/\/images\.melonsour\.com\/[a-zA-Z0-9-.]+/g
   return markdown.match(pattern)
 }
 
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -22,7 +22,7 @@ function Projects() {
         />
         <MediumCard
           link="/post/idol-sort"
-          imgSrc="https://d2pdw9m8ako0ty.cloudfront.net/20240531-idolsort.jpeg"
+          imgSrc="https://images.melonsour.com/20240531-idolsort.jpeg"
           name="Idol Sort"
           description="An app that sorts and ranks Idols"
           stack={['Next.js', 'Docker', 'AWS', 'Chakra UI']}
